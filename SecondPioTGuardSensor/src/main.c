c5ab627be4995d439cc719fc330ad7732d83812a
David Ruiz <daavidruiz01@outlook.com>
2024-04-09 16:37:52 +0200

    prueba main

--- SecondPioTGuardSensor/src/main.c
+++ SecondPioTGuardSensor/src/main.c
@@ -17,7 +17,7 @@
 #include <cJSON.h>
 #include "esp_system.h" // Para esp_chip_info
 #include "mbedtls/debug.h"
-
+//prueba main
 
 static const char *TAG_MQTT = "MQTT";
 static const char *TAG_WIFI = "Wifi";
